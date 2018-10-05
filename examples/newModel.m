838129b618308940b676519ffcd8c6eb79bea6e4
Samuel Harding <harding.samuel.m@gmail.com>
2018-10-05 16:24:49 -0400

    streamlining to make the creation of new models easier

--- examples/newModel.m
+++ examples/newModel.m
@@ -1,4 +1,4 @@
-function O = ex1(varargin)
+function O = newModel(varargin)
 
 if nargin == 0
   OPmode = 'init';
@@ -10,19 +10,21 @@ switch OPmode
   case 'init'
     M = MODEL(mfilename);
     
-    M.data = normrnd(2,0.25,[100,1]);
+    M.data = [];
     
-    M.bayes.priors(1,:) = {'normal',0,10};
-    M.bayes.priors(2,1:3) = {'normal',0,10};
+    M.bayes.priors(1,:) = {[],[],[],    [],[],    [],[]};
+    
+    M.settings.n.chains = 1;
+    M.settings.n.steps = 10000;
+    M.settings.n.groups = 1;
     
     O = M;
   case 'fit'
     params = varargin{2};
     data = varargin{3};
     
+    O = [];
     
-    params(2) = logistic(params(2));
-    O = MODEL.calc_safeLL(normpdf(data,params(1),params(2))); 
     
   case 'pred'
     O = nan;
